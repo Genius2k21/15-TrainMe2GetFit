560059cf1393434a4d68fe364cb76d32c4d42502
Skylar Harwell <swharwell@gmail.com>
2021-09-25 13:33:00 -0400

    Added file structure

---
+++ config/connecion.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+sequelize.sync({force: false})
+
+module.exports = sequelize;
