e67b05160c773f24e5f6a322acf5baaf0c2954c1
unknown <uekemike@yahoo.com>
2021-10-03 17:39:05 -0400

    merging with main

--- public/js/login.js
+++ public/js/login.js
@@ -1,54 +1,35 @@
+const userData = require('../../seeds/user-data.json')
+
 const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#pass').value.trim();
+
   
-    if (email && password) {
+    if (username && password) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        // If successful, redirect the browser to the profile page
-        document.location.replace('/profile');
+        // If successful, redirect the browser to the dashboard page
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
+        //alert('Invalid User!, Please sign up to Login')
       }
     }
   };
   
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ name, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
   
   document
-    .querySelector('.login-form')
+    .querySelector('.validate-form')
     .addEventListener('submit', loginFormHandler);
   
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+
   
--- public/js/logout.js
+++ public/js/logout.js
@@ -11,5 +11,7 @@ const logout = async () => {
     }
   };
   
-  document.querySelector('#logout').addEventListener('click', logout);
+  document
+  .querySelector('#logout')
+  .addEventListener('click', logout);
   
