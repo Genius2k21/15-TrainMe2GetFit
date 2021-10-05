07e4d4740bf1cac57df1c9c19dec2b818e8d0133
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-05 18:42:11 -0400

    closer to having a functional front end

--- public/js/addclient.js
+++ public/js/addclient.js
@@ -18,77 +18,87 @@ $(function(){
         },
         onStepChanging: function (event, currentIndex, newIndex) { 
             event.preventDefault();
-            const firstName = document.querySelector('#first_name');
-            const lastName = document.querySelector('#last_name');
-            const email = document.querySelector('#email');
-            const phone = document.querySelector('#phone');
-            const streetAddress1 = document.querySelector('#address_line_1');
-            const streetAddress2 = document.querySelector('#address_line_2');
-            const city = document.querySelector('#city');
-            const state = document.querySelector('#state');
-            const zipcode = document.querySelector('#zipcode');
-            const gender = $('form input[type=radio]:checked').val();
-            const height_ft = document.querySelector('#height_ft');
-            const height_in = document.querySelector('#height_in');
-            const weight = document.querySelector('#weight');
-            const goal_weight = document.querySelector('#goal_weight');
-            const shoulder = document.querySelector('#shoulder');
-            const chest = document.querySelector('#chest');
-            const waist = document.querySelector('#waist');
-            const hips = document.querySelector('#hips');
-            const left_thigh = document.querySelector('#left_thigh');
-            const right_thigh = document.querySelector('#right_thigh');
-            const left_calf = document.querySelector('#left_calf');
-            const right_calf = document.querySelector('#right_calf');
+            const firstNameEl = document.querySelector('#first_name');
+            const lastNameEl = document.querySelector('#last_name');
+            const emailEl = document.querySelector('#email');
+            const phoneEl = document.querySelector('#phone');
+            const birthdateEl = document.querySelector('#birthdate');
+            const streetAddress1El = document.querySelector('#address_line_1');
+            const streetAddress2El = document.querySelector('#address_line_2');
+            const cityEl = document.querySelector('#city');
+            const stateEl = document.querySelector('#state');
+            const zipcodeEl = document.querySelector('#zipcode');
+            const genderEl = $('form input[type=radio]:checked').val();
+            const height_ftEl = document.querySelector('#height_ft');
+            const height_inEl = document.querySelector('#height_in');
+            const weightEl = document.querySelector('#weight');
+            const goal_weightEl = document.querySelector('#goal_weight');
+            const shoulderEl = document.querySelector('#shoulder');
+            const chestEl = document.querySelector('#chest');
+            const waistEl = document.querySelector('#waist');
+            const hipsEl = document.querySelector('#hips');
+            const left_thighEl = document.querySelector('#left_thigh');
+            const right_thighEl = document.querySelector('#right_thigh');
+            const left_calfEl = document.querySelector('#left_calf');
+            const right_calfEl = document.querySelector('#right_calf');
 
             console.log(`what is the event? ${event}`)
 
             if(currentIndex === 0){
                 let flag = true;
-                if(firstName.value === ''){
+                if(firstNameEl.value === ''){
                     console.log('First name is required');
-                    firstName.classList.add('required');
+                    firstNameEl.classList.add('required');
                     flag = false;
                 }
-                if(lastName.value === ''){
+                if(lastNameEl.value === ''){
                     console.log('Last name is required');
-                    lastName.classList.add('required');
+                    lastNameEl.classList.add('required');
                     flag = false;
                 }
-                if(email.value === ''){
+                if(birthdateEl.value === ''){
+                    birthdateEl.classList.add('required');
+                    flag = false;
+                }
+                if(emailEl.value === ''){
                     console.log('email is required');
-                    email.classList.add('required');
+                    emailEl.classList.add('required');
                     flag = false;
                 }
-                if(phone.value === ''){
+                if(phoneEl.value === ''){
                     console.log('phone is required');
-                    phone.classList.add('required');
+                    phoneEl.classList.add('required');
                     flag = false;
                 }
-
                 return flag;
             }
            
             if(currentIndex === 1){
-                if(weight.value === ''){
+                let flag = true;
+                if(weightEl.value === ''){
                     console.log('weight is required');
-                    return false;
+                    weightEl.classList.add('required');
+                    flag = false;
                 }
-                if(goal_weight.value === ''){
+                if(goal_weightEl.value === ''){
                     console.log('goal_weight is required');
-                    return false;
+                    weightEl.classList.add('required');
+                    flag = false;
                 }
+                if(flag === false){
+                    return flag;
+                }
+                //else no reason to stop execution
             }
-
-            const fullname = firstName.value.trim()+' '+lastName.value.trim();
+            debugger
+            const fullname = firstNameEl.value.trim()+' '+lastNameEl.value.trim();
 
             $('#fullname-val').text(fullname);
-            $('#email-val').text(email.value.trim());
-            $('#phone-val').text(phone.value.trim());
-            $('#gender-val').text(gender);
-            $('#weight-val').text(weight.value.trim());
-            $('#goal-weight-val').text(goal_weight.value.trim());
-
+            $('#email-val').text(emailEl.value.trim());
+            $('#phone-val').text(phoneEl.value.trim());
+            $('#gender-val').text(genderEl);
+            $('#weight-val').text(weightEl.value.trim());
+            $('#goal-weight-val').text(goal_weightEl.value.trim());
             return true;      
         },
         onFinish: function(event){
