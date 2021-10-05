6ccb24f26f93d057c897ca061d7f0dd1814a2203
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-10-05 13:02:42 -0400

    modified to use html5 form controls. required and pattern not firing for some reason

--- db/trainme_alter_schema.sql
+++ db/trainme_alter_schema.sql
@@ -1,11 +1,11 @@
-USE DATABASE trainme_db;
-
-DROP PROCEDURE sp_getUser;
-
+USE trainme_db;
+DROP PROCEDURE IF EXISTS sp_getUser;
+DELIMITER //
 CREATE PROCEDURE `sp_getUser`(IN username VARCHAR(25), IN password VARCHAR(25))
 BEGIN
-	SELECT id as 'user_id'
-    FROM user 
+    SELECT id as 'user_id'
+    FROM user
     WHERE username = username
     AND password = password;
-END
+END //
+DELIMITER  ;
--- public/js/addclient.js
+++ public/js/addclient.js
@@ -1,3 +1,8 @@
+debugger
+
+/**
+ * Out of the box function for wizard functionality
+ */
 $(function(){
     $("#form-total").steps({
         headerTag: "h2",
@@ -14,27 +19,74 @@ $(function(){
             current : ''
         },
         onStepChanging: function (event, currentIndex, newIndex) { 
-            const fullname = $('#first_name').val() + ' ' + $('#last_name').val();
+            event.preventDefault();
+            const firstName = $('#first_name').val() 
+            const lastName = $('#last_name').val();
             const email = $('#email').val();
             const phone = $('#phone').val();
-            const address = $('#address').val();
+            const streetAddress1 = $('#address_line_1').val();
+            const streetAddress2 = $('#address_line_2').val();
+            const city = $('#city').val();
+            const state = $('#state').val();
+            const zipcode = $('#zipcode').val();
             const gender = $('form input[type=radio]:checked').val();
-            const account_name = $('#account_name').val();
-            const account_number = $('#account_number').val();
+            const height_ft = $('#height_ft').val();
+            const height_in = $('#height_in').val();
+            const weight = $('#weight').val();
+            const goal_weight = $('#goal_weight').val();
+            const shoulder = $('#shoulder').val();
+            const chest = $('#chest').val();
+            const waist = $('#waist').val();
+            const hips = $('#hips').val();
+            const left_thigh = $('#left_thigh').val();
+            const right_thigh = $('#right_thigh').val();
+            const left_calf = $('#left_calf').val();
+            const right_calf = $('#right_calf').val();
+
+            const fullname = firstName+' '+lastName;
 
             $('#fullname-val').text(fullname);
             $('#email-val').text(email);
             $('#phone-val').text(phone);
-            $('#address-val').text(address);
             $('#gender-val').text(gender);
-            $('#account-name-val').text(account_name);
-            $('#account-number-val').text(account_number);
+            $('#weight-val').text(weight);
+            $('#goal-weight-val').text(goal_weight);
 
-            return true;
+            return true;      
+        },
+        onsubmit: function(event, currentIndex, newIndex){
+            event.preventDefault();
+            console.log('does this fire?')
         }
     });
 });
 
-$('.datepicker').datepicker();
 
+
+const addClientHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+  
+    // Gather the data from the form elements on the page
+    
+    if (email && phone && firstName && lastName && gender) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
+  
+//   document
+//     .querySelector('.form-register')
+//     .addEventListener('submit', loginFormHandler);
+  
 
