8b7f27342ebf5a28a8067fbedafbee43a9ace44b
unknown <uekemike@yahoo.com>
2021-10-04 20:16:50 -0400

    update login page to display the dashbaord

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,9 +3,10 @@ const sequelize = require('../../config/connection');
 const { Client, User } = require('../../models');
 
 //GET client information for user and client 
+// --> Route '/api/user/:id
 router.get('/:userid',async(req,res) =>{
    try{
-      res.render("dashboard")
+     // res.render("dashboard")
  
     const userid = req.params.userid;
 
@@ -22,23 +23,58 @@ router.get('/:userid',async(req,res) =>{
 })
 
 //POST user information to create user account
+// Route --> '/api/user/user'
 router.post('/user',async(req,res) =>{
 
      // Log that a POST request was received
-    console.log(`${req.body} request received`);
+    console.log(req.body);
+    console.log(`${req.body.toString} request received`);
 
     const {username, password} = req.body;
     const newUser = {username, password};
 
     // Convert the data to a string so we can save it
-    const userString = JSON.stringify(newReview);
+    const userString = JSON.stringify(newUser);
 
-/*   sequelize.query('CALL sp_createUser(:username, :password)',{replacements: {username: newUser.username, password: newUser.password }}).then(function(response){
-        res.json(response);
-       }).error(function(err){
-          res.json(err);
-   });
-*/
+   //CHECK if USER Exists in DB
+   let foundUser = await sequelize.query(`SELECT * FROM user WHERE username=${JSON.stringify(username)}`)
+   console.log(foundUser);
+   if(foundUser) {
+      // then redirect....
+      res.redirect('/dashboard');
+   } else {
+      console.log("Error... something went wrong");
+      //alert("User does not exist");
+   }
+   
+//  sequelize.query('CALL sp_createUser(:username, :password)',{replacements: {username: newUser.username, password: newUser.password }}).then(function(response){
+//         res.json(response);
+//        }).error(function(err){
+//           res.json(err);
+//    });
+
 })
+
+// Route --> '/api/user/createUser'
+router.post('/createUser', async (req, res) => {
+   // Grab the User submitted data 
+   console.log(req.body);
+   console.log(`${req.body.toString} request received`)
+
+   // Create a temp User Object (JS object)
+   const {username, password, email, firstname, lastname} = req.body;
+   const newUser = {username, password,email,firstname, lastname};
+
+   const userString = JSON.stringify(newUser);
+
+   // Create the USER in the DB 
+   //Check if user exist
+
+   let foundUser = await sequelize.query(`SELECT * FROM user WHERE username=${JSON.stringify(username)}`)
+   console.log(foundUser);
+
+   // If Successful REDIRECT if NOT ...
+
+});
 
 module.exports = router;
