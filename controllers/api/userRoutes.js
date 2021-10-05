80817560de2c680825a7de6e06d7899a758b9cd2
Unicorn Virgo <58121705+unicorninvirgo@users.noreply.github.com>
2021-10-04 21:04:44 -0400

    update to userroutes proc

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,38 +2,52 @@ const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Client, User } = require('../../models');
 
-//GET client information for user and client 
-router.get('/:userid',async(req,res) =>{
 
-    const userid = req.params.userid;
+//POST user information to create user account
+router.post('/' , async(req,res) =>{
 
-    sequelize.query('CALL sp_getClients(:userid)',{replacements: {userid: userid}}).then(function(response){
-        res.json(response);
-       }).error(function(err){
-          res.json(err);
-   });
+   // Log that a POST request was received
+   const username = req.query.username;
+   const password = req.query.password;
 
+ sequelize.query('CALL sp_createUser(:username, :password)',{replacements: {username: username, password: password }}).then(function(response){
+      res.json(response);
+     }).error(function(err){
+        res.json(err);
+  });
 
 })
 
+
 //POST user information to create user account
-router.post('/user',async(req,res) =>{
+router.get('/login' , async(req,res) =>{
 
-     // Log that a POST request was received
-    console.log(`${req.body} request received`);
+   // Log that a POST request was received
+   //console.log(`${req.query.username} request received`);
 
-    const {username, password} = req.body;
-    const newUser = {username, password};
+   const username = req.query.username;
+   const password = req.query.password;
 
-    // Convert the data to a string so we can save it
-    const userString = JSON.stringify(newReview);
+ sequelize.query('CALL sp_getUser(:username, :password)',{replacements: {username: username, password: password}}).then(function(response){
+      res.json(response);
+     }).error(function(err){
+        res.json(err);
+  });
 
-/*   sequelize.query('CALL sp_createUser(:username, :password)',{replacements: {username: newUser.username, password: newUser.password }}).then(function(response){
-        res.json(response);
-       }).error(function(err){
-          res.json(err);
-   });
-*/
+})
+
+
+//GET client information for user and client 
+router.get('/:userid',async(req,res) =>{
+
+   const userid = req.params.userid;
+
+   sequelize.query('CALL sp_getClients(:userid)',{replacements: {userid: userid}}).then(function(response){
+       res.json(response);
+      }).error(function(err){
+         res.json(err);
+  });
+
 })
 
 module.exports = router;
